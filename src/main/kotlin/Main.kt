6c018fda0a6f5db343d0191744fcce0e23cb5bdf
JEONG <phliopox327@naver.com>
2022-11-14 10:03:14 +0900

    사무실 코드 정리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 fun main(args: Array<String>) {
     println("안녕하세요, Shoppi 에 오신 것을 환영합니다!")
     println("쇼핑을 계속하시려면 이름을 입력해주세요 :)")
-<<<<<<< HEAD
 
     val name = readLine()
     println("""
@@ -23,13 +22,4 @@ fun main(args: Array<String>) {
 }
 //isNullOrEmpty() : null이면 false를 반환
 //isNullOrBlank() : null과 공백 모두 false를 반환환
-
-
-    val name = readLine()
-    println("""
-        감사합니다. 반갑습니다, $name 님
-        원하시는 카테고리를 입력해주세요
-        ***============================***
-    """.trimIndent())
-}
 
