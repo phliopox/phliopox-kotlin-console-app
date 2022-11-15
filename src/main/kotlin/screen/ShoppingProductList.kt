460a4262d20c996da85dfde8eb399f811fef529d
JEONG <phliopox327@naver.com>
2022-11-15 15:47:38 +0900

    실습 4-3.프로젝트 전체에서 사용하는 텍스트 const로 분리

--- src/main/kotlin/screen/ShoppingProductList.kt
+++ src/main/kotlin/screen/ShoppingProductList.kt
@@ -1,5 +1,6 @@
 package screen
 
+import LINE_DIVIDER
 import data.CartItems
 import data.Product
 import extensions.getNotEmptyInt
@@ -26,7 +27,7 @@ class ShoppingProductList {
         val categoryProduct = categories[selectedCategory]
         if (!categoryProduct.isNullOrEmpty()) {
             println("""
-                ***=================***
+                $LINE_DIVIDER
                 선택하신 [$selectedCategory] 카테고리 상품입니다.
             """.trimIndent())
 
@@ -45,7 +46,7 @@ class ShoppingProductList {
 
     private fun showCartOption(categoryProducts : List<Product> , selectedCategory: String){
         println("""
-            ***======================***
+            $LINE_DIVIDER
             장바구니에 담을 상품 번호를 선택해주세요.
         """.trimIndent())
 
